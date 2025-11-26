95d83a8a6a6cdf513913d9611140298d15c6a8cb
Sachin Kumar <Sachinjnv100@outlook.com>
2025-11-26 21:56:44 +0530

    added some modules and middlewares

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 require('dotenv').config();
 const connectDB = require("./db/index")
-
-connectDB();
+const app = require("./app");
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`http://localhost:3000`);
+    })
+})
+.catch((err)=>{
+    console.log("db connection failed")
+})
